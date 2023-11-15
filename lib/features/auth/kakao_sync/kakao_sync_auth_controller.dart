f699b733dd8b9b621774301ba4ceb625465d0520
ijonghyeon <academy3746@gmail.com>
2023-11-15 09:10:33 +0900

    31. POST Debugging

--- lib/features/auth/kakao_sync/kakao_sync_auth_controller.dart
+++ lib/features/auth/kakao_sync/kakao_sync_auth_controller.dart
@@ -6,13 +6,13 @@ import 'package:http/http.dart' as http;
 class KakaoSyncAuthController {
   Future<void> sendLoginInfoToServer(Map<String, dynamic> loginInfo) async {
     /// Web Server Endpoint for processing Kakao Sync Authentication
-    const String webServerEndPoint = "https://albup.co.kr/plugin/kakao/redirect_kakao.php";
+    const String redirectURL = "https://albup.co.kr/plugin/kakao/redirect_kakao.php";
 
     print("Server Sending Data: $loginInfo");
 
     /// Send API to Web Server from Client
     var response = await http.post(
-      Uri.parse(webServerEndPoint),
+      Uri.parse(redirectURL),
       headers: {"Content-Type": "application/json"},
       body: json.encode(loginInfo),
     );
@@ -20,9 +20,9 @@ class KakaoSyncAuthController {
     /// Response from Web Server to Client: Just for Debugging
     try {
       if (response.statusCode == 200) {
-        print("POST Succeed: ${json.encode(response.statusCode)}");
+        print("POST Succeed: ${response.statusCode}");
       } else {
-        print("Post Failed: ${json.encode(response.statusCode)}");
+        print("Post Failed: ${response.statusCode}");
       }
     } catch(e) {
       print("웹 서버 통신 오류: $e");
