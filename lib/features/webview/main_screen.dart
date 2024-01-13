22d39f5abdf4d900d18573d87da70c63652471ad
ijonghyeon <academy3746@gmail.com>
2024-01-13 19:04:00 +0900

    카카오 로그인 성공!!!

--- lib/features/webview/main_screen.dart
+++ lib/features/webview/main_screen.dart
@@ -2,7 +2,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'package:albup/features/auth/kakao_sync/kakao_login_process.dart';
 import 'package:albup/features/firebase/fcm_controller.dart';
 import 'package:albup/features/webview/widgets/app_cookie_manager.dart';
 import 'package:albup/features/webview/widgets/app_version_checker.dart';
@@ -19,6 +18,7 @@ import 'package:get_storage/get_storage.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainScreen extends StatefulWidget {
   static const String routeName = "/main";
@@ -50,9 +50,6 @@ class _MainScreenState extends State<MainScreen> {
 
   /// Import Cookie Manager
   AppCookieManager? cookieManager;
-
-  /// Import KaKao Sync Login Process
-  final LoginProcess loginProcess = LoginProcess();
 
   /// Import Back Action Handler
   late final BackActionHandler backActionHandler;
@@ -123,6 +120,15 @@ class _MainScreenState extends State<MainScreen> {
     OpenFile.open(path);
   }
 
+  Future<String> _getUserAgent() async {
+    var userAgent = "undefined";
+
+    userAgent =
+        "Mozilla/5.0 (iPhone; CPU iPhone 0S 10_3_1 like Mac OS X) AppLeWebKit/603.1.30 (KHM, Like Gecko) Version/10.0 Mobile/14304 Safari/602.1";
+
+    return userAgent;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -132,168 +138,160 @@ class _MainScreenState extends State<MainScreen> {
         children: [
           LayoutBuilder(
             builder: (BuildContext context, BoxConstraints constraints) {
-              return WillPopScope(
-                onWillPop: () => backActionHandler.onWillPop(),
-                child: SizedBox(
-                  height: constraints.maxHeight,
-                  child: SafeArea(
-                    child: WebView(
-                      initialUrl: url,
-                      javascriptMode: JavascriptMode.unrestricted,
-                      javascriptChannels: <JavascriptChannel>[
-                        _flutterWebviewProJavascriptChannel(context),
-                      ].toSet(),
-                      onWebResourceError: (error) {
-                        print("Error Code: ${error.errorCode}");
-                        print("Error Description: ${error.description}");
-                      },
-                      onWebViewCreated:
-                          (WebViewController webviewController) async {
-                        _controller.complete(webviewController);
-                        _viewController = webviewController;
+              return FutureBuilder(
+                future: _getUserAgent(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return WillPopScope(
+                      onWillPop: () => backActionHandler.onWillPop(),
+                      child: SizedBox(
+                        height: constraints.maxHeight,
+                        child: SafeArea(
+                          child: WebView(
+                            initialUrl: url,
+                            javascriptMode: JavascriptMode.unrestricted,
+                            javascriptChannels: <JavascriptChannel>[
+                              _flutterWebviewProJavascriptChannel(context),
+                            ].toSet(),
+                            onWebResourceError: (error) {
+                              print("Error Code: ${error.errorCode}");
+                              print("Error Description: ${error.description}");
+                            },
+                            onWebViewCreated:
+                                (WebViewController webviewController) async {
+                              _controller.complete(webviewController);
+                              _viewController = webviewController;
 
-                        /// Back Gesture when Webview resources created
-                        backActionHandler = BackActionHandler(
-                          context,
-                          _viewController,
-                          url,
-                        );
+                              /// Back Gesture when Webview resources created
+                              backActionHandler = BackActionHandler(
+                                context,
+                                _viewController,
+                                url,
+                              );
 
-                        /// Application exit or not
-                        webviewController.currentUrl().then((url) {
-                          if (url == "$url") {
-                            setState(() {
-                              isInMainPage = true;
-                            });
-                          } else {
-                            setState(() {
-                              isInMainPage = false;
-                            });
-                          }
-                        });
+                              /// Application exit or not
+                              webviewController.currentUrl().then((url) {
+                                if (url == "$url") {
+                                  setState(() {
+                                    isInMainPage = true;
+                                  });
+                                } else {
+                                  setState(() {
+                                    isInMainPage = false;
+                                  });
+                                }
+                              });
 
-                        /// Cookie Management
-                        await cookieManager?.setCookies(
-                          cookieManager!.cookieValue,
-                          cookieManager!.domain,
-                          cookieManager!.cookieValue,
-                          cookieManager!.url,
-                        );
-                      },
-                      onPageStarted: (String url) async {
-                        setState(() {
-                          isLoading = true;
-                        });
-                        print("Current Page: $url");
-                      },
-                      onPageFinished: (String url) async {
-                        setState(() {
-                          isLoading = false;
-                        });
+                              /// Cookie Management
+                              await cookieManager?.setCookies(
+                                cookieManager!.cookieValue,
+                                cookieManager!.domain,
+                                cookieManager!.cookieValue,
+                                cookieManager!.url,
+                              );
+                            },
+                            onPageStarted: (String url) async {
+                              setState(() {
+                                isLoading = true;
+                              });
+                              print("Current Page: $url");
+                            },
+                            onPageFinished: (String url) async {
+                              setState(() {
+                                isLoading = false;
+                              });
 
-                        /// Android Soft Keyboard 가림 현상 조치
-                        /// window.scrollBy(0, 350);
-                        if (Platform.isAndroid) {
-                          if (url.contains(url) && _viewController != null) {
-                            await _viewController!.runJavascript("""
-                              (function() {
-                                function scrollToFocusedInput(event) {
-                                  const focusedElement = document.activeElement;
-                                  if (focusedElement.tagName.toLowerCase() === 'input' || focusedElement.tagName.toLowerCase() === 'textarea') {
-                                    setTimeout(() => {
-                                      focusedElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
-                                    }, 500);
+                              /// Android Soft Keyboard 가림 현상 조치
+                              /// window.scrollBy(0, 350);
+                              if (Platform.isAndroid) {
+                                if (url.contains(url) &&
+                                    _viewController != null) {
+                                  await _viewController!.runJavascript("""
+                                    (function() {
+                                      function scrollToFocusedInput(event) {
+                                        const focusedElement = document.activeElement;
+                                        if (focusedElement.tagName.toLowerCase() === 'input' || focusedElement.tagName.toLowerCase() === 'textarea') {
+                                          setTimeout(() => {
+                                            focusedElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                                          }, 500);
+                                        }
+                                      }
+                                      document.addEventListener('focus', scrollToFocusedInput, true);
+                                    })();
+                                  """);
+                                }
+                              }
+                            },
+                            navigationDelegate:
+                                (NavigationRequest request) async {
+                              if (request.url.startsWith("kakaotalk://")) {
+                                /// 카카오톡 로그인 Request
+                                if (await isKakaoTalkInstalled()) {
+                                  try {
+                                    await launchUrl(Uri.parse(request.url));
+
+                                    print("Request Kakao Talk Login");
+                                  } catch (e) {
+                                    print("카카오톡 로그인 요청 실패: $e");
+
+                                    /// 사용자에 의한 취소 처리 Handling
+                                    if (e is PlatformException &&
+                                        e.code == 'CANCELED') {
+                                      print("카카오톡 로그인 요청 취소");
+
+                                      return NavigationDecision.prevent;
+                                    }
+
+                                    /// 카카오톡 미설치 시, 카카오 계정으로 로그인
+                                    try {
+                                      print("카카오 계정으로 로그인");
+
+                                      await UserApi.instance
+                                          .loginWithKakaoAccount();
+                                    } catch (e) {
+                                      print("카카오 계정 로그인 실패: $e");
+                                    }
+                                  }
+                                } else {
+                                  /// 카카오톡 미설치 시, 카카오 계정으로 로그인
+                                  try {
+                                    print("카카오 계정으로 로그인");
+
+                                    await UserApi.instance
+                                        .loginWithKakaoAccount();
+                                  } catch (e) {
+                                    print("카카오 계정 로그인 실패: $e");
                                   }
                                 }
-                                document.addEventListener('focus', scrollToFocusedInput, true);
-                              })();
-                      """);
-                          }
-                        }
-                      },
-                      navigationDelegate: (NavigationRequest request) async {
-                        if (kDebugMode) {
-                          /// Kakao Sync TAG
-                          List<String> serviceTerms = [
-                            'account_email',
-                            'name',
-                            'phone_number',
-                          ];
-
-                          if (request.url.contains(
-                              "https://kauth.kakao.com/oauth/authorize")) {
-                            /// 카카오톡 로그인 Request
-                            if (await isKakaoTalkInstalled()) {
-                              try {
-                                OAuthToken token = await UserApi.instance
-                                    .loginWithKakaoTalk(
-                                        serviceTerms: serviceTerms);
-
-                                loginProcess.onLoginSuccess(
-                                  {
-                                    "access_token": token.accessToken,
-                                    "refresh_token": token.refreshToken,
-                                    "id_token": token.idToken,
-                                    "scope": token.scopes,
-                                  },
-                                );
-
-                                print(
-                                    "Request Kakao Talk Login Process to Web Server");
-                              } catch (e) {
-                                print("카카오톡 로그인 요청 실패: $e");
-
-                                /// 사용자에 의한 취소 처리 Handling
-                                if (e is PlatformException &&
-                                    e.code == 'CANCELED') {
-                                  print("카카오톡 로그인 요청 취소");
-
-                                  return NavigationDecision.prevent;
-                                }
-
-                                /// 카카오톡 미설치 시, 카카오 계정으로 로그인
-                                try {
-                                  print("카카오 계정으로 로그인");
-
-                                  await UserApi.instance
-                                      .loginWithKakaoAccount();
-                                } catch (e) {
-                                  print("카카오 계정 로그인 실패: $e");
-                                }
+                                return NavigationDecision.prevent;
                               }
-                            } else {
-                              /// 카카오톡 미설치 시, 카카오 계정으로 로그인
-                              try {
-                                print("카카오 계정으로 로그인");
 
-                                await UserApi.instance.loginWithKakaoAccount();
-                              } catch (e) {
-                                print("카카오 계정 로그인 실패: $e");
+                              if (request.url
+                                  .contains("/data/judgment_pdf_drunk/")) {
+                                await _downloadFile(request.url);
+
+                                return NavigationDecision.prevent;
                               }
-                            }
-                            return NavigationDecision.prevent;
-                          }
-                        }
 
-                        if (request.url.contains("/data/judgment_pdf_drunk/")) {
-                          await _downloadFile(request.url);
-
-                          return NavigationDecision.prevent;
-                        }
-
-                        return NavigationDecision.navigate;
-                      },
-                      zoomEnabled: false,
-                      gestureRecognizers: Set()
-                        ..add(
-                          Factory<EagerGestureRecognizer>(
-                            () => EagerGestureRecognizer(),
+                              return NavigationDecision.navigate;
+                            },
+                            zoomEnabled: false,
+                            gestureRecognizers: Set()
+                              ..add(
+                                Factory<EagerGestureRecognizer>(
+                                  () => EagerGestureRecognizer(),
+                                ),
+                              ),
+                            gestureNavigationEnabled: true,
+                            userAgent: snapshot.data,
                           ),
                         ),
-                      gestureNavigationEnabled: true,
-                    ),
-                  ),
-                ),
+                      ),
+                    );
+                  } else {
+                    return const CircularProgressIndicator.adaptive();
+                  }
+                },
               );
             },
           ),
